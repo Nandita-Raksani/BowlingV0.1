c10b48a4f8627fa17eb632c068fd94f7399f5fad
Nandita <nrak15@gmail.com>
2020-02-16 19:13:55 +0530

    Refactored the game instantiation into a reusable method

--- src/test/java/com/kata/practise/BowlingGameTest.java
+++ src/test/java/com/kata/practise/BowlingGameTest.java
@@ -1,20 +1,26 @@
 package com.kata.practise;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class BowlingGameTest {
+    BowlingGame game;
+
+    @Before
+    public void shouldInitializeBowlingGame(){
+        game = new BowlingGame();
+    }
+
     @Test
     public void shouldStartGameWithScore0OnFirstRoll() {
-        BowlingGame game = new BowlingGame();
         game.roll(0);
         assertEquals(0, game.getScore());
     }
 
     @Test
     public void scoreShouldBe1When1PinKnockedOnFirstRoll() {
-        BowlingGame game = new BowlingGame();
         game.roll(1);
         assertEquals(1, game.getScore());
     }
